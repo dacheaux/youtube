37ae6f0725fd5301d304fb8e71cb88994305d3c2
dacheaux <damjanspace@gmail.com>
2018-10-08 17:47:55 +0200

    tweaks and heroku

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-console.log(path);
-
 app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
